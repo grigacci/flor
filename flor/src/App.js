f2cd8b17f3bcecf44a19b19a3c67bc50c07ad7bd
Guilherme <guilhermerigacci@ufmg.com.br>
2021-04-28 15:42:31 -0300

    Pagina de login + HTML + CSS

--- flor/src/App.js
+++ flor/src/App.js
@@ -1,5 +1,7 @@
-import './App.css';
+import './global.css';
 import Routes from "./routes";
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return <Routes />
--- flor/src/pages/Login/Login.js
+++ flor/src/pages/Login/Login.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import { Link} from "react-router-dom";
+import "./Login.css";
+import {Form , Button} from "react-bootstrap";
 
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-            <Link to="home">Home</Link>
+        <div className="fundo" style={{ backgroundImage: "url(/images/loginback.jpg)" ,
+        backgroundRepeat: 'repeat'}}>
+            
+        <div className="base">
+        <div className="containe">
+            <h1>Flor de lissssssss</h1>
+            <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Insira o email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="Senha" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Lembrar de mim" />
+            </Form.Group>
+            <div className="meio">
+            <Button variant="outline-primary" type="submit" size="lg" block>
+                Entrar
+            </Button>
+            </div>
+            </Form>
+        </div>
+        </div>
         </div>
     )
 }
