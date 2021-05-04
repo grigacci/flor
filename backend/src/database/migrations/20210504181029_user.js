3c6c6ad654a9ab6fb083f3cbd58abfdb6b751bb3
Guilherme <guilhermerigacci@ufmg.com.br>
2021-05-04 16:22:12 -0300

    Criação do banco de dados
    E importação dos módulos

---
+++ backend/src/database/migrations/20210504181029_user.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('user', function(table) {
+      table.string('user_id').primary().notNullable();
+      table.string('name').notNullable();
+      table.string('email').notNullable();
+      table.string('endereco').notNullable();
+      table.string('cidade').notNullable();
+      table.string('estado').notNullable();
+      table.integer('cep').notNullable();
+      table.string('flor').notNullable();
+      table.integer('cartao');
+      table.integer('mes');
+      table.integer('ano');
+      table.integer('cdd');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('user');
+};
---
+++ backend/src/database/migrations/20210504191007_comentarios.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('comentario', function(table) {
+        table.integer('produto_id').notNullable();
+        table
+            .foreign("produto_id")
+            .references("produto_id")
+            .inTable("produto")
+            .onDelete("cascade")
+
+        table.string('user_id').notNullable();
+        table
+            .foreign("user_id")
+            .references("user_id")
+            .inTable("user")
+            .onDelete("cascade")
+        
+        table.string('comentario').notNullable();
+        table.integer('avaliacao').notNullable();
+        
+    })
+  };
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('comentario');
+};
