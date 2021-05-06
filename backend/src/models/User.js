1d3b6204a8e9a6c895ddc63ed48c1bbed6ae8d9f
Lucas José <lucasoliveira@cpejr.com.br>
2021-05-06 17:53:15 -0300

    Correção de erro no updatebyID

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,7 +2,7 @@ const connection = require("../database/connection");
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
-    async create() {
+    async create(user) {
         const user_id = uuidv4();
         user.user_id = user_id;
         const result = await connection("user").insert(user);
@@ -15,7 +15,7 @@ module.exports = {
     },
 
     async updateById(user_id, user){
-        const result =  await connection("user").where(user_id).update(user);
+        const result =  await connection("user").where({user_id}).update(user);
         return result;
     },
 
