ea84ec91c656e18dd5120d2ea8de68cc1b05175d
Lucas José <lucasoliveira@cpejr.com.br>
2021-05-12 19:36:58 -0300

    Banco de dados + alterações_Lista + Criação_modal + criação_produto

--- flor/src/pages/Lista/Lista.js
+++ flor/src/pages/Lista/Lista.js
@@ -1,32 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import "./Lista.css";
 import { Container } from '@material-ui/core';
 import { useHistory } from 'react-router';
 import { ButtonToolbar, ButtonGroup, Col, Image, Row } from 'react-bootstrap';
 import api from '../../services/api';
+import Produto from "../Produto"
 
 const useStyles = makeStyles({
-  root: { maxWidth: 345, },
+  root: { width: "100%", display: "flex", flexDirection: "column", flexGrow: "1", height: "100%" },
 });
-
 
 const _produtos = []
 
+
 function Lista() {
+
   const history = useHistory();
   const classes = useStyles();
   const [produto, setProdutos] = useState(_produtos);
+  console.log(produto.indexOf);
 
-
-  async function getProdutos(){
+  async function getProdutos() {
     try {
       const response = await api.get("/produto");
       setProdutos([...response.data])
@@ -38,7 +35,8 @@ function Lista() {
 
   useEffect(() => {
     getProdutos();
-  },[])
+  }, [])
+
 
 
   return (
@@ -112,7 +110,7 @@ function Lista() {
         }}
         >
           <div className="categoriasList">
-            <p style={{marginBottom: "8px", fontFamily: "Abhaya Libre Regular" }}>Como você vai fazer alguém feliz hoje?</p>
+            <p style={{ marginBottom: "8px", fontFamily: "Abhaya Libre Regular" }}>Como você vai fazer alguém feliz hoje?</p>
             <Button style={{ fontSize: "0.7rem", fontFamily: "Abhaya Libre Regular" }} onClick={() => history.push("/home")}>Cestas</Button>
             <Button style={{ fontSize: "0.7rem", fontFamily: "Abhaya Libre Regular" }} onClick={() => history.push("/home")}>Buquês</Button>
             <Button style={{ fontSize: "0.7rem", fontFamily: "Abhaya Libre Regular" }} onClick={() => history.push("/home")}>Sementes</Button>
@@ -121,50 +119,19 @@ function Lista() {
             <Button style={{ fontSize: "0.7rem", fontFamily: "Abhaya Libre Regular" }} onClick={() => history.push("/home")}>Vaso de Flores</Button>
           </div>
         </Card>
+
         <div className="baseList">
           <div className="elementosList">
-            {produto.map((listItem, index) => {
-              var nome = `/images/${listItem.produto_id}.jpg`;
-              return (
-                <div className="boxList">
-                  <Card className={classes.root}>
-                    <CardActionArea >
-                      <CardMedia
-                        component="img"
-                        alt={listItem.name}
-                        height="140"
-                        image={nome}
-                        title={listItem.name}
-                      />
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          {listItem.name}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                          {listItem.descrição}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                          R${listItem.preco}
-                                </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                      <Button size="small" color="secundary">
-                        Colocar no carrinho
-                            </Button>
-                    </CardActions>
-                  </Card>
-                </div>
-              );
-            })}
+            <Produto/>
           </div>
         </div>
       </div>
+
       <div className="proxPaginaList">
-      <ButtonToolbar aria-label="Toolbar with button groups" style={{
-          display:"flex",
-          justifyContent:"center",
-          width:"100%",
+        <ButtonToolbar aria-label="Toolbar with button groups" style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
         }}
         >
           <ButtonGroup className="mr-2List" aria-label="Second group">
