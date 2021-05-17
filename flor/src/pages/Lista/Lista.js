27312a9cdce567e491a0c7581d3c52fc78a913cb
Guilherme <guilhermerigacci@ufmg.com.br>
2021-05-17 20:09:01 -0300

    Commit obrigatorio

--- flor/src/pages/Lista/Lista.js
+++ flor/src/pages/Lista/Lista.js
@@ -1,32 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles, Card, Button, Container, CardActionArea, CardMedia, CardContent, Typography } from '@material-ui/core/';
 import "./Lista.css";
-import { Container } from '@material-ui/core';
 import { useHistory } from 'react-router';
 import { ButtonToolbar, ButtonGroup, Col, Image, Row } from 'react-bootstrap';
 import api from '../../services/api';
+import ProdutoModal from "../ProdutoModal"
+import Modal from 'react-bootstrap/Modal'
+
 
 const useStyles = makeStyles({
-  root: { maxWidth: 345, },
+  root: { width: "100%", display: "flex", flexDirection: "column", flexGrow: "1", height: "100%" },
 });
-
 
 const _produtos = []
 
+
 function Lista() {
+
   const history = useHistory();
   const classes = useStyles();
   const [produto, setProdutos] = useState(_produtos);
+  const [produtoAtual, setProdutoAtual] = useState();
+  const [modalShow, setModalShow] = React.useState(false);
+  const [show, setOpen] = useState(false);
+  console.log(produtoAtual);
 
 
-  async function getProdutos(){
+  async function getProdutos() {
     try {
       const response = await api.get("/produto");
       setProdutos([...response.data])
@@ -38,9 +38,24 @@ function Lista() {
 
   useEffect(() => {
     getProdutos();
-  },[])
+  }, [])
+
+  function handleClick(product) {
+    setOpen(true);
+    setProdutoAtual(product);
+  }
+
+  function teste() {
+    if (show === true) {
+      console.log(produtoAtual.name);
+      return produtoAtual.name;
+    }
+  }
 
 
+  function handleClose() {
+    setOpen(false);
+  }
   return (
     <div className="incluiTodosList" style={{
       backgroundImage: "url(/images/fundocaixatextos.png)",
@@ -112,7 +127,7 @@ function Lista() {
         }}
         >
           <div className="categoriasList">
-            <p style={{marginBottom: "8px", fontFamily: "Abhaya Libre Regular" }}>Como você vai fazer alguém feliz hoje?</p>
+            <p style={{ marginBottom: "8px", fontFamily: "Abhaya Libre Regular" }}>Como você vai fazer alguém feliz hoje?</p>
             <Button style={{ fontSize: "0.7rem", fontFamily: "Abhaya Libre Regular" }} onClick={() => history.push("/home")}>Cestas</Button>
             <Button style={{ fontSize: "0.7rem", fontFamily: "Abhaya Libre Regular" }} onClick={() => history.push("/home")}>Buquês</Button>
             <Button style={{ fontSize: "0.7rem", fontFamily: "Abhaya Libre Regular" }} onClick={() => history.push("/home")}>Sementes</Button>
@@ -121,50 +136,75 @@ function Lista() {
             <Button style={{ fontSize: "0.7rem", fontFamily: "Abhaya Libre Regular" }} onClick={() => history.push("/home")}>Vaso de Flores</Button>
           </div>
         </Card>
+
         <div className="baseList">
           <div className="elementosList">
             {produto.map((listItem, index) => {
               var nome = `/images/${listItem.produto_id}.jpg`;
               return (
-                <div className="boxList">
-                  <Card className={classes.root}>
-                    <CardActionArea >
-                      <CardMedia
-                        component="img"
-                        alt={listItem.name}
-                        height="140"
-                        image={nome}
-                        title={listItem.name}
+                <>
+                  <Button key={index} onClick={() => handleClick(listItem)}>
+                    <div className="boxListProd" >
+                      <Card className={classes.root} >
+                        <CardActionArea style={{ display: "flex", flexDirection: "column", flexGrow: "1" }} >
+                          <CardMedia
+                            component="img"
+                            alt={listItem.name}
+                            height="160"
+                            image={nome}
+                            title={listItem.name}
+                            style={{ width: "auto", alignSelf: "center" }}
+                          />
+                          <CardContent style={{ display: "flex", flexDirection: "column", flexGrow: "1" }}>
+                            <Typography gutterBottom style={{
+                              fontSize: "1.16rem",
+                              fontFamily: "Roboto",
+                              fontWeight: "400",
+                              lineHeight: "1.334",
+                              letterSpacing: "0em",
+                            }}>
+                              {listItem.name}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                              {listItem.descrição}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p" style={{ alignSelf: "center", marginTop: "auto" }}>
+                              R${listItem.preco}
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+                    </div>
+                  </Button>
+
+                  <div className="modal">
+                    <Modal
+                      centered
+                      className="modalList"
+                      show={show}
+                      style={{ display:"flex" ,backgroundImage: "url(/images/fundo.png)" }}
+                    >
+                      <ProdutoModal
+                        show={show}
+                        onHide={handleClose}
+                        data={produtoAtual}
+
                       />
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          {listItem.name}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                          {listItem.descrição}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                          R${listItem.preco}
-                                </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                      <Button size="small" color="secundary">
-                        Colocar no carrinho
-                            </Button>
-                    </CardActions>
-                  </Card>
-                </div>
+                    </Modal>
+                  </div>
+
+                </>
               );
             })}
           </div>
         </div>
       </div>
+
       <div className="proxPaginaList">
-      <ButtonToolbar aria-label="Toolbar with button groups" style={{
-          display:"flex",
-          justifyContent:"center",
-          width:"100%",
+        <ButtonToolbar aria-label="Toolbar with button groups" style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
         }}
         >
           <ButtonGroup className="mr-2List" aria-label="Second group">
@@ -176,7 +216,7 @@ function Lista() {
           </ButtonGroup>
         </ButtonToolbar>
       </div>
-    </div>
+    </div >
   );
 }
 
