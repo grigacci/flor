88889c0c40267383a4fbf90388914b11487cb586
Lucas José <lucasoliveira@cpejr.com.br>
2021-05-22 16:10:10 -0300

    Centralizei os produtos da lista

--- flor/src/pages/components/Produto/Produto.js
+++ flor/src/pages/components/Produto/Produto.js
@@ -5,6 +5,7 @@ import api from '../../../services/api';
 import Form from 'react-bootstrap/Form'
 import { makeStyles } from '@material-ui/core/styles';
 import ProdutoModal from "../ProdutoModal"
+import { Col } from "react-bootstrap";
 
 const _produtos = []
 
@@ -53,82 +54,83 @@ function Produto() {
   return (
     <div className="countainerProd" >
 
-      <div>
-        <Form.Group controlId="formBasicSelect">
-          <Form.Control
-            as="select"
-            value={type}
-            onChange={(e) => { onChange(e.target.value) }}
-          >
-            <option value="Todas as categorias">Todas as categorias</option>
-            <option value="Cestas de café" >Cestas de café</option>
-            <option value="Buquê" >Buquê</option>
-            <option value="Arranjos" >Arranjos</option>
-            <option value="Flores variadas" >Flores variadas</option>
-          </Form.Control>
+      <div className="filtroProd">
+      <Form.Group controlId="formBasicSelect">
+            <Form.Control
+              as="select"
+              value={type}
+              onChange={(e) => { onChange(e.target.value) }}
+            >
+              <option value="Todas as categorias">Todas as categorias</option>
+              <option value="Cestas de café" >Cestas de café</option>
+              <option value="Buquê" >Buquê</option>
+              <option value="Arranjos" >Arranjos</option>
+              <option value="Flores variadas" >Flores variadas</option>
+            </Form.Control>
         </Form.Group>
       </div>
       <div className="listProd">
         {produto &&
-          produto.map((listItem, index) =>  {
+          produto.map((listItem, index) => {
             var nome = `/images/${listItem.produto_id}.jpg`;
-            if (listItem.categoria === type || type === 'Todas as categorias'){
+            if (listItem.categoria === type || type === 'Todas as categorias') {
 
-            return (
-              <>
-                <div className="elementosProd">
-                  <Button key={index} onClick={() => handleClick(listItem)}>
-                    <div className="boxListProd" >
-                      <Card className={classes.root} >
-                        <CardActionArea style={{ display: "flex", flexDirection: "column", flexGrow: "1" }} >
-                          <CardMedia
-                            component="img"
-                            alt={listItem.name}
-                            height="160"
-                            image={nome}
-                            title={listItem.name}
-                            style={{ width: "auto", alignSelf: "center" }}
-                          />
-                          <CardContent style={{ display: "flex", flexDirection: "column", flexGrow: "1" }}>
-                            <Typography gutterBottom style={{
-                              fontSize: "1.16rem",
-                              fontFamily: "Roboto",
-                              fontWeight: "400",
-                              lineHeight: "1.334",
-                              letterSpacing: "0em",
-                            }}>
-                              {listItem.name}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                              {listItem.descrição}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p" style={{ alignSelf: "center", marginTop: "auto" }}>
-                              R${listItem.preco}
-                            </Typography>
-                          </CardContent>
-                        </CardActionArea>
-                      </Card>
-                    </div>
-                  </Button>
-                </div>
+              return (
+                <>
+                  <div className="elementosProd">
+                    <Button key={index} onClick={() => handleClick(listItem)}>
+                      <div className="boxListProd" >
+                        <Card className={classes.root} >
+                          <CardActionArea style={{ display: "flex", flexDirection: "column", flexGrow: "1" }} >
+                            <CardMedia
+                              component="img"
+                              alt={listItem.name}
+                              height="160"
+                              image={nome}
+                              title={listItem.name}
+                              style={{ width: "auto", alignSelf: "center" }}
+                            />
+                            <CardContent style={{ display: "flex", flexDirection: "column", flexGrow: "1" }}>
+                              <Typography gutterBottom style={{
+                                fontSize: "1.16rem",
+                                fontFamily: "Roboto",
+                                fontWeight: "400",
+                                lineHeight: "1.334",
+                                letterSpacing: "0em",
+                              }}>
+                                {listItem.name}
+                              </Typography>
+                              <Typography variant="body2" color="textSecondary" component="p">
+                                {listItem.descrição}
+                              </Typography>
+                              <Typography variant="body2" color="textSecondary" component="p" style={{ alignSelf: "center", marginTop: "auto" }}>
+                                R${listItem.preco}
+                              </Typography>
+                            </CardContent>
+                          </CardActionArea>
+                        </Card>
+                      </div>
+                    </Button>
+                  </div>
 
-                <div className="modal">
-                  <Modal
-                    className="modalList"
-                    open={show}
-                    onClose={handleClose}
-                    style={{ opacity: "1" }}
-                    BackdropProps={{ style: { backgroundColor: "rgba(0, 0, 0, 0" } }}
-                  >
-                    <ProdutoModal
+                  <div className="modal">
+                    <Modal
+                      className="modalList"
                       open={show}
                       onClose={handleClose}
-                      data={produtoAtual}
-                    />
-                  </Modal>
-                </div>
-              </>
-            );}
+                      style={{ opacity: "1" }}
+                      BackdropProps={{ style: { backgroundColor: "rgba(0, 0, 0, 0" } }}
+                    >
+                      <ProdutoModal
+                        open={show}
+                        onClose={handleClose}
+                        data={produtoAtual}
+                      />
+                    </Modal>
+                  </div>
+                </>
+              );
+            }
           })}
       </div>
     </div>
